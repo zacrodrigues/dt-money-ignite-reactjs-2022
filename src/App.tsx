919c45b7a63b80301b2a6c475f84dddf647473bd
Zac Rodrigues <rodrigues.contano@gmail.com>
2023-01-17 21:06:00 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { Transations } from "./pages/Transactions";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      
+      <Transations />
+    </ThemeProvider>
+  )
+}
